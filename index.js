0332afe9ee02d33045b47cee2801deda38e49887
Sailesh1078 <125793897+Sailesh1078@users.noreply.github.com>
2023-02-19 15:49:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const body = document.body;
+let lastScroll = 0;
+
+ window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+        if (currentScroll <= 0) {
+            body.classList.remove("scroll-up");
+            return;
+        }
+
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        } else if (
+            currentScroll < lastScroll &&
+            body.classList.contains("scroll-down")
+        ) {
+            body.classList.remove("scroll-down");
+            body.classList.add("scroll-up");
+        }
+        lastScroll = currentScroll;
+});
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
